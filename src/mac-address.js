3b34ed60e013c5453c2a3f21489697f7dbb0f81a
Leniliya <antonova.aliona2010@yandex.ru>
2023-04-16 20:16:09 +0300

    mac address: done

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  let arr = n.split('-');
+  let isHexaxdemical = (str) => {
+    let first = str.toString().codePointAt();
+    let second = str.toString().codePointAt();
+    if (first >= 48 && first <= 70) {
+      if (second >= 48 && second <= 70) {
+        return true
+      }
+    }
+    return false
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length !== 2) {
+      return false
+    }
+    if (!isHexaxdemical(arr[i])) {
+      return false
+    }
+  }
+  return true
 }
 module.exports = {
   isMAC48Address
