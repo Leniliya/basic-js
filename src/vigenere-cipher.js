f20fc5391d4bce9cc9101b76bcb30c795af02c2d
Leniliya <antonova.aliona2010@yandex.ru>
2023-04-17 19:21:34 +0300

    vinegere cipher: done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value) {
+    this.direct = value;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let arrMess = message.toUpperCase().split('');
+    let keyIndex = 0;
+    for (let i = 0; i < arrMess.length; i++) {
+      if (arrMess[i].codePointAt() >= 65 && arrMess[i].codePointAt() <= 90) {
+        let char = arrMess[i].codePointAt() - 65;
+        let keyChar = key[keyIndex].toUpperCase().codePointAt() - 65;
+        let resChar = char + keyChar;
+        if (resChar < 26) {
+          arrMess[i] = String.fromCodePoint(resChar + 65);
+        } else {
+          arrMess[i] = String.fromCodePoint(resChar - 26 + 65);
+        }
+        keyIndex++;
+        if (keyIndex === key.length) {
+          keyIndex = 0;
+        }
+      }
+    }
+    if (this.direct === false) {
+      return arrMess.reverse().join('');
+    } else {
+      return arrMess.join('');
+    }
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let arrMess = encryptedMessage.toUpperCase().split('');
+    let keyIndex = 0;
+    for (let i = 0; i < arrMess.length; i++) {
+      if (arrMess[i].codePointAt() >= 65 && arrMess[i].codePointAt() <= 90) {
+        let char = arrMess[i].codePointAt() - 65;
+        let keyChar = key[keyIndex].toUpperCase().codePointAt() - 65;
+        let resChar = char - keyChar;
+        if (resChar < 0) {
+          arrMess[i] = String.fromCodePoint(resChar + 26 + 65);
+        } else {
+          arrMess[i] = String.fromCodePoint(resChar + 65);
+        }
+        keyIndex++;
+        if (keyIndex === key.length) {
+          keyIndex = 0;
+        }
+      }
+    }
+    if (this.direct === false) {
+      return arrMess.reverse().join('');
+    } else {
+      return arrMess.join('');
+    }
   }
 }
 
